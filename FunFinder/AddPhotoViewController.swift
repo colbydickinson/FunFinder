1b4a281c3793eacf7b16925b4227099bd64dcdaa
Colby Dickinson <colby.dickinson@cox.net>
2019-05-18 14:46:03 -0600

    Initial Commit

---
+++ FunFinder/AddPhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddPhotoViewController.swift
+//  FunFinder
+//
+//  Created by Colby Dickinson on 5/18/19.
+//  Copyright © 2019 Colby Dickinson. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
